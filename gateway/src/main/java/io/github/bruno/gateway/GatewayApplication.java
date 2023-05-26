f99aaa6a44955642169194581fb26467317e1a63
Bruno Santos <bruno_st17@hotmail.com>
2023-05-26 10:18:11 -0300

    Adicionando o ms gateway

---
+++ gateway/src/main/java/io/github/bruno/gateway/GatewayApplication.java
@@ -0,0 +1,24 @@
+package io.github.bruno.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+    @Bean
+    public RouteLocator router(RouteLocatorBuilder routerLocator) {
+        return routerLocator.routes()
+                .route(r -> r.path("/v1/**").uri("lb://ms-client"))
+                .build();
+    }
+}
