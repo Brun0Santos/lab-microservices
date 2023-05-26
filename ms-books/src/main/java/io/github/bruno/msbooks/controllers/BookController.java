7312bd3dab25df25fe8309c0df485a3c7081ead5
Bruno Santos <bruno_st17@hotmail.com>
2023-05-26 10:58:05 -0300

    Configurando as rotas

--- ms-books/src/main/java/io/github/bruno/msbooks/controllers/BookController.java
+++ ms-books/src/main/java/io/github/bruno/msbooks/controllers/BookController.java
@@ -1,6 +1,9 @@
 package io.github.bruno.msbooks.controllers;
 
 import io.github.bruno.msbooks.entities.BookEntity;
+import io.github.bruno.msbooks.services.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,8 +12,11 @@ import java.util.List;
 @RestController
 @RequestMapping("/books")
 public class BookController {
+    @Autowired
+    private BookService service;
 
-    public List<BookEntity> getAllBooks() {
+    @GetMapping
+    public List<BookEntity> returnAllBook() {
 
     }
 }
--- ms-books/src/main/java/io/github/bruno/msbooks/services/BookService.java
+++ ms-books/src/main/java/io/github/bruno/msbooks/services/BookService.java
@@ -1,9 +1,22 @@
 package io.github.bruno.msbooks.services;
 
+import io.github.bruno.msbooks.entities.BookEntity;
+import io.github.bruno.msbooks.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class BookService {
+    @Autowired
+    private BookRepository repository;
 
+    public List<BookEntity> getAllBooks() {
+        return repository.findAll();
+    }
 
+    public BookEntity getFromId(Integer id) {
+        return repository.findById(id).orElseThrow();
+    }
 }
