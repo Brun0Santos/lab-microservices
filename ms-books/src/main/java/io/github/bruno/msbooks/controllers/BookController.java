d47a92e50ed59d772523bec50e269f133127cf48
Bruno Santos <bruno_st17@hotmail.com>
2023-05-26 11:01:06 -0300

    Finalizando as rotas

--- ms-books/src/main/java/io/github/bruno/msbooks/controllers/BookController.java
+++ ms-books/src/main/java/io/github/bruno/msbooks/controllers/BookController.java
@@ -3,7 +3,9 @@ package io.github.bruno.msbooks.controllers;
 import io.github.bruno.msbooks.entities.BookEntity;
 import io.github.bruno.msbooks.services.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,7 +18,15 @@ public class BookController {
     private BookService service;
 
     @GetMapping
-    public List<BookEntity> returnAllBook() {
+    public ResponseEntity<List<BookEntity>> returnAllBook() {
+        List<BookEntity> allBooks = service.getAllBooks();
+        return ResponseEntity.ok(allBooks);
+    }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<BookEntity> returnSpecificUser(@RequestBody Integer id) {
+        BookEntity specificUser = service.getFromId(id);
+        return ResponseEntity.ok(specificUser);
     }
 }
+
