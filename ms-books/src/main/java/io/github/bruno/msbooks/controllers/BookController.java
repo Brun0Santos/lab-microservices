f83870b85a11c1a8428b77250e5711497b0d6720
Bruno Santos <bruno_st17@hotmail.com>
2023-05-29 09:43:16 -0300

    Configurando o cors de forma global

--- ms-books/src/main/java/io/github/bruno/msbooks/controllers/BookController.java
+++ ms-books/src/main/java/io/github/bruno/msbooks/controllers/BookController.java
@@ -4,10 +4,7 @@ import io.github.bruno.msbooks.entities.BookEntity;
 import io.github.bruno.msbooks.services.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
